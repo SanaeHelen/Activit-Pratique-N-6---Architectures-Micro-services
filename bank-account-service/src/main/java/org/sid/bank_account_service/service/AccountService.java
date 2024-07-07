a28113446e9cc0d25462a737ae1b032fafbcd814
SanaeHelen <yousfisanae3@gmail.com>
2024-07-07 23:30:41 +0100

    GraphQL

--- bank-account-service/src/main/java/org/sid/bank_account_service/service/AccountService.java
+++ bank-account-service/src/main/java/org/sid/bank_account_service/service/AccountService.java
@@ -5,4 +5,6 @@ import org.sid.bank_account_service.dto.BankAccountResponseDTO;
 
 public interface AccountService {
      BankAccountResponseDTO addAccout(BankAccountRequestDTO bankAccountDTO);
+
+     BankAccountResponseDTO updateAccout(String id, BankAccountRequestDTO bankAccountDTO);
 }
--- bank-account-service/src/main/java/org/sid/bank_account_service/service/AccountServiceImpl.java
+++ bank-account-service/src/main/java/org/sid/bank_account_service/service/AccountServiceImpl.java
@@ -35,4 +35,18 @@ public class AccountServiceImpl implements AccountService {
         return bankAccountResponseDTO;
 
     }
+    @Override
+    public BankAccountResponseDTO updateAccout(String id ,BankAccountRequestDTO bankAccountDTO) {
+        BankAccount bankAccount=BankAccount.builder()
+                .id(id)
+                .createdAt(new Date())
+                .balance(bankAccountDTO.getBalance())
+                .type(bankAccountDTO.getType())
+                .currency(bankAccountDTO.getCurrency())
+                .build();
+        BankAccount saveBankAccount = bankAccountRepository.save(bankAccount) ;
+        BankAccountResponseDTO bankAccountResponseDTO = accountMapper.fromBankAccount(saveBankAccount);
+        return bankAccountResponseDTO;
+
+    }
 }
